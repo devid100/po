2a016197792b7cecadb9e06ee2446789762ba18e
devid100 <82916614+devid100@users.noreply.github.com>
2021-05-05 05:09:43 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -11605,8 +11605,7 @@ end
 ---------------------- الاوامر الجديدة
 if text == 'الاوامر' then
 if not Mod(msg) then
-send(msg.chat_id_, msg.id_,' ❃∫︙ هاذا الامر خاص بالادمنيه
-❃∫︙ ارسل {م10} لعرض اوامر الاعضاء ')
+send(msg.chat_id_, msg.id_,' هاذا الامر خاص بالادمنيه فقط ')
 return false
 end
 if AddChannel(msg.sender_user_id_) == false then
